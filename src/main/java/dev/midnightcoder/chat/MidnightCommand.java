74afe7200062f32167a6f164bea96db5a95fe9a1
Glabay <Java_Guru@live.com>
2026-04-15 20:57:12 -0300

    Initial commit

---
+++ src/main/java/dev/midnightcoder/chat/MidnightCommand.java
@@ -0,0 +1,9 @@
+package dev.midnightcoder.chat;
+
+/**
+ * @author Glabay | Glabay-Studios
+ * @project MidnightChat
+ * @social Discord: Glabay
+ * @since 2026-04-07
+*/public class MidnightCommand {
+}
